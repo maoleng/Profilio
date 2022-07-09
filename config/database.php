e323a9cb7e4a26a06a87cd0d17338971f0322a74
Bùi Hữu Lộc <feature451@gmail.com>
2022-07-10 06:19:02 +0700

    Config MongoDB

--- config/database.php
+++ config/database.php
@@ -35,6 +35,12 @@ return [
 
     'connections' => [
 
+        'mongodb' => [
+            'driver' => 'mongodb',
+            'dsn' => env('DB_URI'),
+            'database' => env('DB_DATABASE'),
+        ],
+
         'sqlite' => [
             'driver' => 'sqlite',
             'url' => env('DATABASE_URL'),
